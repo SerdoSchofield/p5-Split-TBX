75981a761d1fd460e8fb53cabd69b646af61b759
SerdoSchofield <james.s.hayes@gmail.com>
2014-07-07 13:30:48 -0600

    more writing, currently reads in binary

--- lib/Read/TBX.pm
+++ lib/Read/TBX.pm
@@ -4,27 +4,29 @@ package Read::TBX;
 use strict;
 use warnings;
 use XML::Simple;
-use Test::XML;
 use open ':encoding(utf8)', ':std';
 
 our $VERSION = 0.01;
 
+_run(@ARGV) unless caller();
+
 sub readTBX
 {
-	$fh = shift;
-	testXML($fh);
+	my $fh = shift;
+	_testXML($fh);
 }
 
 sub _run
 {
-	@ARGV >= 1 or die "Usage: perl TBX.pm <input TBX file>";
-	testXML($ARGV[0]);
+	(@_ >= 1) or die "Usage: perl TBX.pm <input TBX file> at lib/Read/TBX.pm line 19.";
+	my $fh = $_[0];
+	_testXML($fh);
 }
 
 sub _handle
 {
 	## no critic (RequireBriefOpen)
-    my ( $fh ) = @_;
+    my $fh = shift;
     my $handle;
     if ($fh) {
         if ( ref($fh) eq 'GLOB' ) {
@@ -35,41 +37,36 @@ sub _handle
 			$handle = \*STDIN;
 		}
         else {
-            open my $fh2, '<', $fh or die "Couldn't open $fh";
-            $handle = $fh2;
+            open $handle, '<', $fh or die "Couldn't open $fh";
         }
     }
     return $handle;
 }
 
 
-sub _testXML;
+sub _testXML
 {
-	my $fh = shift;  
+	my $fh = shift; 
 	$fh = _handle($fh); #convert $fh into filehandle if it isn't already
-	
-	if (!is_good_xml($fh))
-	{
-		_readTBXBinary($fh);
-	}
+
+	_readTBXBinary($fh)
 }
 
 sub _readTBXBinary
 {
-	$fh = shift;
-	$fh = binmode($fh);
-	my $byteCount = undef;
+	my $fh = shift;
+	binmode($fh);
+	my $capture;
+	my $termEntryCapture;
 	
-	while(read($fh, $byteCount, 0))
-	{
-		$byteCount++;
-		print "\n".$byteCount;
+	while(read($fh, my $byteCount, 1000))
+	{	
+
+		
 	}
 }
 
 sub _readTBXSimple
 {
-	$fh = shift;
+	my $fh = shift;
 }
-
-_run() unless caller();
